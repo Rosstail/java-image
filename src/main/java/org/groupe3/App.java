dc68d61d8de5d22348d607db81fe1f3fc9ca7b3d
Anthony Bac <anthonybac@MacBook-Pro-de-admin-4.local>
2019-12-18 10:36:02 +0100

    added grayScaleFilter

---
+++ src/main/java/org/groupe3/App.java
@@ -0,0 +1,10 @@
+package org.groupe3;
+
+
+public class App {
+    public static void main(String[] args) {
+        JavaHelper javaHelper = new JavaHelper();
+        //javaHelper.exempleBlurFile();
+        javaHelper.filGrayScale();
+    }
+}
--- src/main/java/org/groupe3/JavaHelper.java
+++ src/main/java/org/groupe3/JavaHelper.java
@@ -10,28 +10,25 @@ import java.io.File;
 
 import static org.bytedeco.opencv.global.opencv_imgproc.*;
 
-​
-        ​
-        ​
 public class JavaHelper {
-​
+
     public void exempleBlurFile() {
-        File f = new File("imgs/test.jpg");
+        File f = new File("/Users/anthonybac/Desktop/java-image/src/main/java/org/groupe3/imgs/test.jpg");
         Mat image = opencv_imgcodecs.imread(f.getAbsolutePath());
         image = filterBlur(image);
-​
-        File outputDir = new File("output");
+
+        File outputDir = new File("/Users/anthonybac/Desktop/java-image/src/main/java/org/groupe3/imgs");
         File outputFile = new File(outputDir, "result.jpg");
         opencv_imgcodecs.imwrite(outputFile.getAbsolutePath(), image);
     }
-​
+
     public Mat filterBlur(Mat image) {
         int size = 3;
         Mat result = image.clone();
         GaussianBlur(image, result, new Size(size, size), 0);
         return result;
     }
-​
+
     public Mat filterDilate(Mat image) {
         int size = 8;
         Mat result = image.clone();
@@ -39,10 +36,21 @@ public class JavaHelper {
         dilate(image, result, element);
         return result;
     }
-​
+
+    public void filGrayScale() {
+        File f = new File("/Users/anthonybac/Desktop/java-image/src/main/java/org/groupe3/imgs/test.jpg");
+        Mat image = opencv_imgcodecs.imread(f.getAbsolutePath());
+        image = filterGrayscale(image);
+
+        File outputDir = new File("/Users/anthonybac/Desktop/java-image/src/main/java/org/groupe3/imgs");
+        File outputFile = new File(outputDir, "result.jpg");
+        opencv_imgcodecs.imwrite(outputFile.getAbsolutePath(), image);
+    }
+
     public Mat filterGrayscale(Mat image) {
         Mat result = new Mat(image.rows(), image.cols(), CvType.CV_8UC3);
         cvtColor(image, result, Imgproc.COLOR_RGB2GRAY);
         return result;
     }
+
 }
